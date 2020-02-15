e7e4dc1f405f99778095a2fc9dcf8d073aa79947
rajatthakur00@gmail.com <rajat@55>
2020-02-15 16:47:58 +0530

    test_commit

---
+++ src/main/java/com/example/mcroservices_project/McroservicesProjectApplication.java
@@ -0,0 +1,14 @@
+package com.example.mcroservices_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class McroservicesProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(McroservicesProjectApplication.class, args);
+        System.out.println("***Starting Micro");
+    }
+
+}
---
+++ src/test/java/com/example/mcroservices_project/McroservicesProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mcroservices_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class McroservicesProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
