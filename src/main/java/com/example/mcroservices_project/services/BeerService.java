5e901654817e72b2dc2cbbc78bbf566e7fa3d326
rajatthakur00@gmail.com <rajat@55>
2020-02-16 14:14:30 +0530

    Deleting Beer Handler only

--- src/main/java/com/example/mcroservices_project/services/BeerService.java
+++ src/main/java/com/example/mcroservices_project/services/BeerService.java
@@ -10,4 +10,6 @@ public interface BeerService {
     BeerDto saveNewBeer();
 
     void updateBeer(UUID beerId, BeerDto beerDto);
+
+    void deleteById(UUID beerId);
 }
--- src/main/java/com/example/mcroservices_project/services/BeerServiceImpl.java
+++ src/main/java/com/example/mcroservices_project/services/BeerServiceImpl.java
@@ -1,11 +1,13 @@
 package com.example.mcroservices_project.services;
 
 import com.example.mcroservices_project.web.model.BeerDto;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.UUID;
 
 @Service
+@Slf4j
 public class BeerServiceImpl implements BeerService {
     @Override
     public BeerDto getBeerById(UUID beerId) {
@@ -24,5 +26,11 @@ public class BeerServiceImpl implements BeerService {
     @Override
     public void updateBeer(UUID beerId, BeerDto beerDto) {
         //todo impl
+    }
+
+    @Override
+    public void deleteById(UUID beerId) {
+        //todo impl
+        log.debug("deleting beer" + beerId);
     }
 }
